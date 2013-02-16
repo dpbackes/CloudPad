abbd540f0b4a1176172d87bba771bae20119aae5
dpbackes <dbackes@gmail.com>
2013-02-16 12:14:06 -0600

    Remove logging, version protection

--- cloudpad.js
+++ cloudpad.js
@@ -1,41 +1,72 @@
+var version = 0;
+var canUpdate = true;
 window.onload = function() {
   		
 				
-			
-				$.ajax({
-					url: docURL,
-					type: 'GET',
-					data: { _apikey: apiKey
-								},
-					success: function(response) { 
-										$("body").html(response.text);
-									},
-					error: function(stuff) { 
-								
-								}
-				});
-			
-			
-				var updateFunc = function()
-				{
-						console.log($("body").html());
+	//load the document from the mongodb		
+	$.ajax({
+		url: docURL,
+		type: 'GET',
+		data: { 
+			_apikey: apiKey
+		},
+		success: function(response) { 
+			$("body").html(response.text);
+			version = response.version || 0;
+		},
+		error: function(response) { 
+					
+		}
+	});
+	
+	//updates the document with the current text	
+	var updateFunc = function()
+	{
+		if(canUpdate)
+		{
+			$.ajax({
+				url: docURL,
+				type: 'GET',
+				data: { 
+					_apikey: apiKey
+				},
+				success: function(response) { 
+					//check to make sure the version hasn't been
+					//changed by another CloudPad instance
+					if(resposne.version == version)
+					{
+						version = version + 1;
+						
 						$.ajax({
 							url: docURL,
 							type: 'PUT',
-							data: { _apikey: apiKey,
-										document: { "text" : $("body").html() }  
-										},
+							data: { 
+								_apikey: apiKey,
+								document: { "text" : $("body").html(), "version" : version }  
+							},
 							success: function(resposne) { 
-												console.log("success\n");
-											},
+								
+							},
 							error: function(stuff) { 
-										console.log("error\n");
-										
-										}
+								
+							}
 						});
+					}
+					else
+					{
+						canUpdate = false;
+						$("body").text("CloudPad has been changed in another editor, please refresh your browser");
+					}
+				},
+				error: function(response) { 
+							
 				}
-				
-				$(document).keyup(updateFunc);
-				
-				$(window).unload(updateFunc);
-			}
+			});
+		}
+	}
+	
+	$(document).keyup(updateFunc);
+	
+	$(window).unload(updateFunc);
+			
+}
