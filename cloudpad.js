ce54fb428c4f94d07c9367f9c16a8248d2e08097
Dan Backes <dan.backes@carefusion.com>
2013-02-16 12:38:51 -0600

    Versioning was trickier than I thought. Rolling back to non-versioned scheme. Fix formating and removed logging

--- cloudpad.js
+++ cloudpad.js
@@ -1,82 +1,35 @@
-var version = 0;
-var canUpdate = true;
-var typingTimer;
 window.onload = function() {
-  		
-				
-	//load the document from the mongodb		
 	$.ajax({
 		url: docURL,
 		type: 'GET',
-		data: { 
-			_apikey: apiKey
-		},
+		data: { _apikey: apiKey
+					},
 		success: function(response) { 
 			$("body").html(response.text);
-			version = response.version || 0;
 		},
-		error: function(response) { 
+		error: function(stuff) { 
 					
 		}
 	});
-	
-	//updates the document with the current text	
+
+
 	var updateFunc = function()
 	{
-		if(canUpdate)
-		{
+			console.log($("body").html());
 			$.ajax({
 				url: docURL,
-				type: 'GET',
-				data: { 
-					_apikey: apiKey
+				type: 'PUT',
+				data: { _apikey: apiKey,
+					document: { "text" : $("body").html() }  
 				},
-				success: function(response) { 
-					//check to make sure the version hasn't been
-					//changed by another CloudPad instance
-					if(response.version == version || !(response.version))
-					{
-						version = version + 1;
-						
-						$.ajax({
-							url: docURL,
-							type: 'PUT',
-							data: { 
-								_apikey: apiKey,
-								document: { "text" : $("body").html(), "version" : version }  
-							},
-							success: function(resposne) { 
-							},
-							error: function(stuff) { 
-							}
-						});
-					}
-					else
-					{
-						canUpdate = false;
-						$("body").text("CloudPad has been changed in another editor, please refresh your browser");
-					}
+				success: function(resposne) {
 				},
-				error: function(response) { 	
+				error: function(stuff) { 				
 				}
 			});
-		}
 	}
 	
-	var typingDone = function()
-	{
-		typingTimer = setTimeout(updateFunc, 500);
-	}
-	
-	var typingStart = function()
-	{
-		clearTimeout(typingTimer);
-	}
-	
-	$(document).keyup(typingDone);
-	
-	$(document).keydown(typingStart);
+	$(document).keyup(updateFunc);
 	
 	$(window).unload(updateFunc);
-			
 }
